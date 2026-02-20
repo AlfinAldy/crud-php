e78a39c07537f3912d0b881a394558580138e527
AlfinAldy <alfinaldi077@gmail.com>
2026-02-20 21:28:37 +0700

    Add files via upload

---
+++ config/database3.php
@@ -0,0 +1,20 @@
+<?php
+//set time zone ke zona asia/jakarta
+date_default_timezone_set('Asia/Jakarta');
+//Memulai Sesion 
+session_start();
+
+class Database
+{
+    public $host = "localhost";
+    public $username = "root";
+    public $pass = "";
+    public $db = "clothes";
+    public $connect;
+
+    function __construct()
+    {
+        $this->connect = mysqli_connect($this->host, $this->username, $this->pass, $this->db);
+    }
+}
+?>
